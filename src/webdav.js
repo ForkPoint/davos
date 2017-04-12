e95a1682f964bd0b021fd3355d719d5c16d3e897
Ivan Popjelev <ivan@forkpoint.com>
2017-04-12 12:50:32 +0300

    refactoring webdav

--- src/webdav.js
+++ src/webdav.js
@@ -29,7 +29,7 @@
       jar: false
     });
 
-  // Local dependencies
+  // Locals
   const configHelper = require('./config'),
     log = require('./logger');
 
@@ -38,8 +38,6 @@
    * @param {Object} config The configuration object used by Davos
    */
   var WebDav = function(config) {
-
-    //Initialize
 
     configHelper.validateConfigProperties(config);
 
@@ -53,91 +51,92 @@
       timeout: REQUEST_TIMEOUT
     };
 
-    //Private
-
     function getUriPath(targetPath) {
-      let i,
-        previous,
-        current,
-        dirs = targetPath.split(path.sep),
-        len = dirs.length;
-      for (i = 0; i < len; i += 1) {
-        current = dirs[i];
+      let dirs = targetPath.split(path.sep),
+        len = dirs.length,
+        previous;
+
+      for (let i = 0; i < len; i++) {
+        let current = dirs[i];
+
         if (current === 'cartridge') {
           break;
         }
+
         previous = current;
       }
       return '/' + targetPath.slice(targetPath.indexOf(previous));
     }
 
     function doRequest(options, path, attemptsLeft, retryDelay, reject, resolve, lastError) {
-      var req = null,
+      let req = null,
         stream = null;
+
+      if (attemptsLeft <= 0) {
+        let e = (lastError !== null ? lastError : new Error('The request to ' + path + ' could not be processed!'));
+        reject(e);
+        return;
+      }
 
       if (attemptsLeft === MAX_ATTEMPTS) {
         options = Object.assign(options, baseOptions);
       }
 
-      if (attemptsLeft <= 0) {
-        reject((lastError !== null ? lastError : new Error('The request to ' + path + ' could not be processed!')));
-      } else {
+      if (path) {
+        options.uri = getUriPath(path);
+      }
 
-        if(path) {
-          options.uri = getUriPath(path);
+      let signature = options.method + ' :: ' + options.uri;
+
+      req = request(options, function (error, response, body) {
+        if (attemptsLeft < MAX_ATTEMPTS) {
+          log.debug('Trying to ' + signature + ' for the ' + (MAX_ATTEMPTS - attemptsLeft) + ' time out of ' + MAX_ATTEMPTS + ' tries left.');
         }
-        var signature = options.method + ' :: ' + options.uri;
-        req = request(options, function(error, response, body) {
-          if(attemptsLeft < MAX_ATTEMPTS) {
-            log.debug('Trying to ' + signature + ' for the ' + (MAX_ATTEMPTS - attemptsLeft) + ' time out of ' + MAX_ATTEMPTS + ' tries left.');
-          }
 
-          var e = null;
-          if ((error && _.contains(ERR_CODES, error.code))) {
-            log.error('Got ' + error.code + ' scheduling a retry after ' + retryDelay + 'ms');
-            if (stream) {
-              stream.close();
-            }
-            //abort current request
-            req.abort();
-
-            //schedule a retry
-            setTimeout((function() {
-              doRequest(options, path, --attemptsLeft, retryDelay, reject, resolve, e);
-            }), retryDelay);
-
-          } else if (!error && response.statusCode >= 400 && response.statusCode !== 404) {
-            log.error( response.statusMessage + ' ' + response.statusCode + ". Could not " + signature + " :: skipping file.");
-            e = new Error(response.statusMessage + ' ' + response.statusCode);
-            e.code = response.statusCode;
-            return reject(null);
-          } else if (!error && (200 <= response.statusCode && response.statusCode < 300)) {
-            log.debug('Succesfully actioned ' + path);
-            return resolve(body);
-          } else if (!!response && response.statusCode === 404 && options.method === 'DELETE') {
-            log.info('File ' + options.uri + ' was not found, maybe already deleted.');
-            return resolve(body);
-          } else if (error) {
-            e = new Error(error);
-            e.code = error.code;
-            return reject(e);
-          } else {
-            e = new Error('Unspecified error occurred...' + response.statusCode);
-            e.code = response.statusCode;
-            return reject(e);
+        if (error && _.contains(ERR_CODES, error.code)) {
+          log.error('Got ' + error.code + ' scheduling a retry after ' + retryDelay + 'ms');
+          if (stream) {
+            stream.close();
           }
-        });
-        if (options.method === 'PUT') {
-          try {
-            stream = fs.createReadStream(path);
-            stream.pipe(req);
-            stream.on('end', function() {
-              stream.close();
-            });
-          } catch (e) {
-            log.error('There was an error reading the fs stream from ' + path + ' :: ' + e.code);
-            return reject(e);
-          }
+          // abort current request
+          req.abort();
+          // schedule a retry
+          setTimeout((function () {
+            doRequest(options, path, --attemptsLeft, retryDelay, reject, resolve, e);
+          }), retryDelay);
+        } else if (!error && response.statusCode >= 400 && response.statusCode !== 404) {
+          log.error( response.statusMessage + ' ' + response.statusCode + ". Could not " + signature + " :: skipping file.");
+
+          let e = new Error(response.statusMessage + ' ' + response.statusCode);
+          e.code = response.statusCode;
+          reject(e); // Issue #18 - investigate reason for reject(null);
+        } else if (!error && (200 <= response.statusCode && response.statusCode < 300)) {
+          log.debug('Succesfully actioned ' + path);
+          resolve(body);
+        } else if (response && response.statusCode === 404 && options.method === 'DELETE') {
+          log.info('File ' + options.uri + ' was not found, maybe already deleted.');
+          resolve(body);
+        } else if (error) {
+          let e = new Error(error);
+          e.code = error.code;
+          reject(e);
+        } else {
+          let e = new Error('Unspecified error occurred...' + response.statusCode);
+          e.code = response.statusCode;
+          reject(e);
+        }
+      });
+
+      if (options.method === 'PUT') {
+        try {
+          stream = fs.createReadStream(path);
+          stream.pipe(req);
+          stream.on('end', function () {
+            stream.close();
+          });
+        } catch (e) {
+          log.error('There was an error reading the fs stream from ' + path + ' :: ' + e.code);
+          reject(e);
         }
       }
     }
@@ -145,7 +144,7 @@
     //Public
     return {
       delete: function(path) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
           var options = {
             method: 'DELETE'
           };
@@ -153,7 +152,7 @@
         });
       },
       mkcol: function(path) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
           var options = {
             method: 'MKCOL'
           };
@@ -161,7 +160,7 @@
         });
       },
       put: function(path) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
           var options = {
             method: 'PUT'
           };
@@ -169,7 +168,7 @@
         });
       },
       unzip: function(path) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
           var options = {
             method: 'POST',
             form: {
@@ -180,7 +179,7 @@
         });
       },
       propfind: function() {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
           var options = {
             method: 'PROPFIND'
           };
@@ -189,6 +188,7 @@
       }
     };
   };
+
   module.exports = function(configuration) {
     return new WebDav(configuration);
   };
